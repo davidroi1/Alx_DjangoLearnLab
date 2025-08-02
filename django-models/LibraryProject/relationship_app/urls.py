7996541000d60f4664e5440a1695f2aee2540532
davidroi1 <vagba.belia1@gmail.com>
2025-08-02 17:07:28 +0000

    new submission for all projects

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('libraries/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/register.html'), name='logout'),
     path('register/', views.register, name='register'),
 ]
