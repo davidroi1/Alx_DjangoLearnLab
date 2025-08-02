53f284753cbb9f0131e94eafc2719b78d68269d1
davidroi1 <vagba.belia1@gmail.com>
2025-08-02 06:58:07 +0000

    new submission for all django-model project

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from relationship_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', views.get_all_books, name='list_books'),
+    path('libraries/', views.GetLibraryList.as_view(), name='library_list'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Author, Book, Librarian, Library
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from . import query_samples
 
-# Create your views here.
+
+def get_all_books(request):
+    if request.method == 'GET':
+        try:
+            books = Book.objects.all()
+            return render(request, 'list_books.html', {'books': books})
+        except Book.DoesNotExist:
+            return HttpResponse("No books found.", status=404)
+
+
+class GetLibraryList(ListView):
+    model = Library
+    templat = 'library_detail.html'
+    context = 'libraries'
+
