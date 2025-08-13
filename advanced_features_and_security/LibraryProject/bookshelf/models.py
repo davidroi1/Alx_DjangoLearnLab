3956351c9a231d6f5b3c9b8dba079be0671634ce
davidroi1 <vagba.belia1@gmail.com>
2025-08-13 16:51:12 +0000

    first pushing

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import User
 
 
@@ -55,3 +56,22 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return f"{self.date_of_birth}, {self.profile_photo}"
+    
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('the email field must be set')
+        email = self.normalize_email(email=email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        return self.create_user(email, password, **extra_fields)
