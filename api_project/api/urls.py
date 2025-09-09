a95ea20b48a42bd2d158c25fcfa287b37b2cd1ff
davidroi1 <vagba.belia1@gmail.com>
2025-09-09 14:11:38 +0000

    new version of codebase

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
+from django.urls import include
+from rest_framework.routers import DefaultRouter
 from .views import BookList
+from .views import BookViewSet
+
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book_list')
+    path('api/', include(router.urls)),
+    path('books/', BookList.as_view(), name='book_list'),
 ]
